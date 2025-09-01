6536e80a9a2066ae95c4675711352ccb3d9dd454
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-09-01 19:33:48 +0200

    Enhance sign-in screen layout with image and welcome text
    - Added ScrollView for better content management.
    - Included an image for onboarding and updated text elements for a welcoming message.

--- app/signIn.tsx
+++ app/signIn.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import images from '@/constants/images'
 
 const signIn = () => {
   return (
-    <View>
-      <Text>signIn</Text>
+    <View className='bg-white h-full'>
+      <ScrollView contentContainerClassName='h-full'>
+        <Image source={images.onboarding} className='w-full h-4/6' resizeMode='cover'></Image>
+        <View className='px-10'>
+          <Text className='text-base text-center uppercase font-rubik text-black-200'>Welcome to Restate</Text>
+          <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>Lets Get You Closer To {'\n'}
+            <Text className='text-primary-300'>Your Dream Home</Text>
+          </Text>
+        </View>
+      </ScrollView>
     </View>
   )
 }
