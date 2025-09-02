86797048a503d85629b787be3792a30b2d50b157
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-09-02 20:10:19 +0200

    Update dependencies and enhance sign-in functionality
    - Added .env.local to .gitignore for environment variable management.
    - Updated package.json and package-lock.json to include expo-web-browser, react-native-appwrite, and react-native-url-polyfill dependencies.
    - Enhanced sign-in logic to provide user feedback on login success or failure.

--- app/signIn.tsx
+++ app/signIn.tsx
@@ -1,11 +1,18 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, Alert } from 'react-native'
 import React from 'react'
 import images from '@/constants/images'
 import icons from '@/constants/icons'
+import { login } from './lib/appwrite'
 
 const signIn = () => {
-  const handleLogin = () => {
-    console.log('Login')
+  const handleLogin = async () => {
+    const result = await login();
+
+    if (result) {
+      console.log('Login successful');
+    } else {
+      Alert.alert('Error', 'Login failed');
+    }
   }
   return (
     <View className='bg-white h-full'>
