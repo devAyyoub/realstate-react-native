6568355706e9061d9a9d5006e6731b7818e7cfd7
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-09-02 17:32:09 +0200

    Implement Google sign-in button and update welcome text on sign-in screen
    - Added a TouchableOpacity for Google sign-in functionality.
    - Updated welcome text to "Your Ideal Home" for improved messaging.
    - Included a prompt for users to sign in to continue.

--- app/signIn.tsx
+++ app/signIn.tsx
@@ -1,8 +1,12 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import images from '@/constants/images'
+import icons from '@/constants/icons'
 
 const signIn = () => {
+  const handleLogin = () => {
+    console.log('Login')
+  }
   return (
     <View className='bg-white h-full'>
       <ScrollView contentContainerClassName='h-full'>
@@ -10,8 +14,16 @@ const signIn = () => {
         <View className='px-10'>
           <Text className='text-base text-center uppercase font-rubik text-black-200'>Welcome to Restate</Text>
           <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>Lets Get You Closer To {'\n'}
-            <Text className='text-primary-300'>Your Dream Home</Text>
+            <Text className='text-primary-300'>Your Ideal Home</Text>
           </Text>
+          <Text className='text-lg font-rubik text-black-200 text-center mt-12'>Please sign in to continue</Text>
+          <TouchableOpacity onPress={handleLogin} className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'>
+            <View className='flex flex-row items-center justify-center'>
+              <Image source={icons.google} className='w-5 h-5' resizeMode='contain' />
+              <Text className='text-lg font-rubik-medium text-black-300 ml-2'>Continue with Google</Text>
+            </View>
+          </TouchableOpacity>
+
         </View>
       </ScrollView>
     </View>
